63ca12fe1a8e4363540d60ea95ce954e0180c2b0
Kelvin <vokemalosh32@gmail.com>
2022-07-27 11:36:43 +0300

    Updates

---
+++ precache-manifest.8631fd8cfc197319b2a6c649277b38ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db32e8f4a722ba9d49d43f644300b0f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d99904c606c0bfe7c839",
+    "url": "/static/js/2.dce23e61.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.dce23e61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0679757aaec70f7cbe",
+    "url": "/static/js/main.0b48150e.chunk.js"
+  },
+  {
+    "revision": "7894e797df582624cb7a",
+    "url": "/static/js/runtime-main.36a7665f.js"
+  }
+]);
